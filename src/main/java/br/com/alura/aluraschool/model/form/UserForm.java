da461bcb90717e5726eeace7c4503ce84b05a581
lucasagw <lucas.walter@ucsal.edu.br>
2024-05-13 16:30:30 -0300

    feat: Update field constraints in UserForm class
    Adjust constraints for 'name' and 'username' fields

--- src/main/java/br/com/alura/aluraschool/model/form/UserForm.java
+++ src/main/java/br/com/alura/aluraschool/model/form/UserForm.java
@@ -1,17 +1,17 @@
 package br.com.alura.aluraschool.model.form;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 
 @Getter
 public class UserForm {
 
     @NotBlank
+    @Size(min = 8, message = "Name must have at least 8 characters.")
     private String name;
 
     @Pattern(regexp = "^[a-z]+$", message = "Username must consist only of lowercase letters, without numerals or spaces.")
+    @Size(min = 4, message = "Username must have at least 4 characters.")
     @NotBlank
     private String username;
 
