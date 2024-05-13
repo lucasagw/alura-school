3d5700bb271f8b1b8b3b598942057dd84059dc43
lucasagw <lucas.walter@ucsal.edu.br>
2024-05-13 16:26:17 -0300

    feat: Add new constants to AluraSchoolConstants interface
    Implement additional constants in AluraSchoolConstants interface.

--- src/main/java/br/com/alura/aluraschool/constants/AluraSchoolConstants.java
+++ src/main/java/br/com/alura/aluraschool/constants/AluraSchoolConstants.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 public interface AluraSchoolConstants {
 
     class EmailSend {
-        public static String subjects = "Feedback Unsatisfactoriness";
+
+        public static final String SUBJECTS = "Feedback Unsatisfactoriness";
     }
 }
