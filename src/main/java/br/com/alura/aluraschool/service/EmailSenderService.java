1f8b66badf24cdd555b3553a42a2aa9160a63590
lucasagw <lucas.walter@ucsal.edu.br>
2024-05-12 22:41:14 -0300

    feat: Implement EmailSenderService
    Create EmailSenderService to handle email sending functionality.

---
+++ src/main/java/br/com/alura/aluraschool/service/EmailSenderService.java
@@ -0,0 +1,16 @@
+package br.com.alura.aluraschool.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailSenderService {
+    public static void send(String recipientEmail, String subject, String body) {
+        System.out.println(
+                "Simulating sending email to [%s]:\n".formatted(recipientEmail)
+        );
+        System.out.println("""
+                           Subject: %s
+                            Body: %s
+                           """.formatted(subject, body));
+    }
+}
