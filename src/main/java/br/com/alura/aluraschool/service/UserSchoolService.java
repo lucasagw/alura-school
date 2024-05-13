d92635a3f8912d0d9dd9d5d3fc59020a2128ade3
lucasagw <lucas.walter@ucsal.edu.br>
2024-05-13 18:51:03 -0300

    feat: Rename UserService to UserSchoolService
    Rename UserService to UserSchoolService for clarity and consistency.

--- src/main/java/br/com/alura/aluraschool/service/UserSchoolService.java
+++ src/main/java/br/com/alura/aluraschool/service/UserSchoolService.java
@@ -15,7 +15,7 @@ import java.util.NoSuchElementException;
 import java.util.Set;
 
 @Service
-public class UserService {
+public class UserSchoolService {
 
     private final BCryptPasswordEncoder bCrypt = new BCryptPasswordEncoder();
 
