95aac7f082104a78c413e4043b77266f94914988
lucasagw <lucas.walter@ucsal.edu.br>
2024-05-14 13:48:41 -0300

    feat: Add new constants to AluraSchoolConstants interface
    Implement additional constants in AluraSchoolConstants interface.

--- src/main/java/br/com/alura/aluraschool/constants/AluraSchoolConstants.java
+++ src/main/java/br/com/alura/aluraschool/constants/AluraSchoolConstants.java
@@ -9,4 +9,20 @@ public interface AluraSchoolConstants {
 
         public static final String SUBJECTS = "Feedback Unsatisfactoriness";
     }
+
+    class FeedbackRating {
+
+        public static final int PROMOTERS = 9;
+
+        public static final int DETRACTORS = 6;
+
+        public static int NUMBER_OF_ENROLLMENTS = 4;
+    }
+
+    class Utils {
+
+        public static final double PERCENTAGE = 100.0;
+
+        public static final int INITIAL_VALUE = 0;
+    }
 }
