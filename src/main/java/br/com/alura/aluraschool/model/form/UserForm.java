10622a8abaf6e28df07b02d66f136d0ee31cd7d3
lucasagw <lucas.walter@ucsal.edu.br>
2024-05-13 21:18:14 -0300

    fix: optimize imports

--- src/main/java/br/com/alura/aluraschool/model/form/UserForm.java
+++ src/main/java/br/com/alura/aluraschool/model/form/UserForm.java
@@ -1,6 +1,9 @@
 package br.com.alura.aluraschool.model.form;
 
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
