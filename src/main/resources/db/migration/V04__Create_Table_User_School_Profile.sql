acd1c4e39d09c0edac807484c838caa176a02b5b
lucasagw <lucas.walter@ucsal.edu.br>
2024-05-15 03:36:41 -0300

    fix: Resolve Flyway migration

--- src/main/resources/db/migration/V04__Create_Table_User_School_Profile.sql
+++ src/main/resources/db/migration/V04__Create_Table_User_School_Profile.sql
@@ -2,10 +2,10 @@
 CREATE INDEX idx_email ON user_school (email);
 
 CREATE TABLE IF NOT EXISTS user_school_profile (
-                                                   user_key_username VARCHAR(20) NOT NULL,
-    user_key_email VARCHAR(100) NOT NULL,
+    user_username VARCHAR(20) NOT NULL,
+    user_email VARCHAR(100) NOT NULL,
     profile_id BIGINT NOT NULL,
-    PRIMARY KEY (user_key_username, user_key_email, profile_id),
-    FOREIGN KEY (user_key_username, user_key_email) REFERENCES user_school(username, email),
+    PRIMARY KEY (user_username, user_email, profile_id),
+    FOREIGN KEY (user_username, user_email) REFERENCES user_school(username, email),
     FOREIGN KEY (profile_id) REFERENCES profile(id)
     );
