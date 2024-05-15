2603e21d47641c3eb3aaf4d554af1fb8fdb64520
lucasagw <lucas.walter@ucsal.edu.br>
2024-05-15 12:34:09 -0300

    refactor: Rename columns

--- src/main/resources/db/migration/V03__Create_Table_Course.sql
+++ src/main/resources/db/migration/V03__Create_Table_Course.sql
@@ -2,12 +2,12 @@ CREATE TABLE  IF NOT EXISTS course (
                         id BIGINT NOT NULL AUTO_INCREMENT,
                         name VARCHAR(220) NOT NULL,
                         code VARCHAR(10) UNIQUE NOT NULL,
-                        instructor_name VARCHAR(200) NOT NULL,
+                        instructor_username VARCHAR(200) NOT NULL,
                         instructor_email VARCHAR(100) NOT NULL,
                         description VARCHAR(350) NOT NULL,
                         status BOOLEAN NOT NULL,
                         created_at DATE NOT NULL,
                         deactivated_at DATE,
                         PRIMARY KEY (id),
-                        FOREIGN KEY (instructor_name, instructor_email) REFERENCES user_school(username, email)
+                        FOREIGN KEY (instructor_username, instructor_email) REFERENCES user_school(username, email)
 );
--- src/main/resources/db/migration/V06__Create_Enrollment.sql
+++ src/main/resources/db/migration/V06__Create_Enrollment.sql
@@ -1,10 +1,10 @@
 CREATE TABLE  IF NOT EXISTS enrollment (
                             id BIGINT NOT NULL AUTO_INCREMENT,
-                            student_name VARCHAR(200) NOT NULL,
+                            student_username VARCHAR(200) NOT NULL,
                             student_email VARCHAR(100) NOT NULL,
                             course_id BIGINT NOT NULL,
                             created_at DATE NOT NULL,
                             PRIMARY KEY (id),
-                            FOREIGN KEY (student_name, student_email) REFERENCES user_school(username, email),
+                            FOREIGN KEY (student_username, student_email) REFERENCES user_school(username, email),
                             FOREIGN KEY (course_id) REFERENCES course(id)
 );
