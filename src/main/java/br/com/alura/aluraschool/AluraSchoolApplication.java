16490f8e5eb4cd90cef65a3b8981d04e53cb97eb
lucasagw <lucas.walter@ucsal.edu.br>
2024-05-15 02:09:32 -0300

    chore: Remove insignificant whitespace changes

--- src/main/java/br/com/alura/aluraschool/AluraSchoolApplication.java
+++ src/main/java/br/com/alura/aluraschool/AluraSchoolApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AluraSchoolApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AluraSchoolApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AluraSchoolApplication.class, args);
+    }
 
 }
